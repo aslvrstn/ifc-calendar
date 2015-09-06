7c12bcc34b769522d8da840e7c28f58d8c69ce0d
Alex Silverstein <aslvrstn@gmail.com>
2015-09-06 17:53:45 -0400

    Cleanup

--- scrape.py
+++ scrape.py
@@ -15,15 +15,13 @@ cal.add('version', '2.0')
 for div in soup.find_all('div', id=re.compile('day_.*')):
   partial_date = div.find('h4').string
   showdate = datetime.strptime(partial_date, '%A, %B %d').replace(year=date.today().year)
-  print showdate
   for movie in div.find_all('li'):
     movie_link = movie.a
     movie_title = movie.a.string
-    print movie_title
     for showtime_str in movie.find_all('a', href=re.compile('http://www.movietickets.com/.*')):
       showtime = datetime.strptime(showtime_str.string, '%I:%M %p')
       full_showtime = datetime.combine(showdate.date(), showtime.time())
-      print full_showtime
+
       event = Event()
       event.add('summary', movie_title)
       event.add('dtstart', full_showtime)
