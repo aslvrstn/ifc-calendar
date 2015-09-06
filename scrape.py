427a155966509f4e421c9cbe8f9ebbb59865e302
Alex Silverstein <aslvrstn@gmail.com>
2015-09-06 17:05:08 -0400

    Printing an already useful schedule

--- scrape.py
+++ scrape.py
@@ -3,8 +3,14 @@ import re
 import urllib2
 
 dateless_page = urllib2.urlopen('http://www.ifccenter.com/')
-soup = BeautifulSoup(dateless_page, "html.parser")
+soup = BeautifulSoup(dateless_page, 'html.parser')
 
 for div in soup.find_all('div', id=re.compile('day_.*')):
   date = div.find('h4').string
   print date
+  for movie in div.find_all('li'):
+    movie_link = movie.a
+    movie_title = movie.a.string
+    print movie_title
+    for showtime in movie.find_all('a', href=re.compile('http://www.movietickets.com/.*')):
+      print showtime.string
